a68fc2530213cefc8b3ef55b7f58e4aa4e05e855
Paul Dittaro <pdittaro@gmail.com>
2019-10-18 13:55:54 -0700

    Remove checkout scm in parallel build

--- bin/build_commands.sh
+++ bin/build_commands.sh
@@ -3,3 +3,5 @@ oc new-build https://github.com/pdittaro/devops-platform-workshops-labs#pdittaro
 https://console.pathfinder.gov.bc.ca:8443/apis/build.openshift.io/v1/namespaces/4zq6uj-pdittaro-ocp201-tst-tools/buildconfigs/devops-platform-workshops-labs/webhooks/EPiH9i6TRxa5fYQb7CcQ/github
 
 oc new-build https://github.com/pdittaro/devops-platform-workshops-labs.git#pdittaro-201 --context-dir=blackbox_exporter/.pipeline --name blackbox
+
+# oc delete all,configmaps,templates,pvc,secrets --all
